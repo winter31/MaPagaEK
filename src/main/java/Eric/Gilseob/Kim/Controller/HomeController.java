3ba82f8629362dda98a116c144bfa6e6680047d5
kita <kita@kita-PC>
2017-06-16 17:17:10 +0900

    OCR/ OPENCV need to edit

--- src/main/java/Eric/Gilseob/Kim/Controller/HomeController.java
+++ src/main/java/Eric/Gilseob/Kim/Controller/HomeController.java
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.asprise.ocr.Ocr;
+import com.asprise.ocr.util.OcrLibHelper;
 /**
  * Handles requests for the application home page.
  */
@@ -26,13 +28,27 @@ public class HomeController {
 		return "index_home";
 	}
 	
-	@RequestMapping(value = "/openCV", method = RequestMethod.GET)
+/*	@RequestMapping(value = "/openCV", method = RequestMethod.GET)
 	public String openCV(Model model) {
 		System.load(new File("C:\\Users\\kita\\git\\MaPageEK\\src\\main\\webapp\\resources\\opencv_java320.dll").getAbsolutePath());
 		 Mat mat = new Mat();
 		 Mat m  = Mat.eye(5, 5, CvType.CV_8UC1); 
 		 model.addAttribute("test","m = " + m.dump()); 
 		return "openCV";
-	}
+	}*/
 	
+	
+	@RequestMapping(value = "/ocr", method = RequestMethod.GET)
+	public String ocrTest(Model model) {
+		Ocr.setUp(); // one time setup
+		Ocr ocr = new Ocr(); // create a new OCR engine
+		ocr.startEngine("kor", Ocr.SPEED_FASTEST); // English
+		String s = ocr.recognize(new File[] {new File("C:\\Users\\kita\\Desktop\\test.png")},
+		Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
+	
+		System.out.println(s);
+	
+		ocr.stopEngine(); 
+		return "ocrTest";
+	}
 }
