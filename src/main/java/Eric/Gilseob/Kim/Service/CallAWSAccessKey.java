93dc30e7748c55fdb9d1ab3a7f7874bd1ab55e2e
winter31 <seob31@gmail.com>
2017-11-01 16:25:08 +0900

    IN AWSAccessKey

--- src/main/java/Eric/Gilseob/Kim/Service/CallAWSAccessKey.java
+++ src/main/java/Eric/Gilseob/Kim/Service/CallAWSAccessKey.java
@@ -1,43 +1,43 @@
 package Eric.Gilseob.Kim.Service;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.Reader;
+import java.util.HashMap;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
 
 public class CallAWSAccessKey {
 	
-	private String keyAddress = "c://";
-	private File accessKey = new File(keyAddress);
-	private FileInputStream fis;
-	private ObjectInputStream ois;
+	private String keyAddress = "C:\\Users\\GILSEOB\\Downloads\\rootkey.txt";
+	private BufferedReader bufferReader;
 	
 	public CallAWSAccessKey() {
 		// TODO Auto-generated constructor stub
+	}
+	//Access Key load into Spring for AWS S3
+	public String[] getAccessKey(){
+		String[] accessKeyList = new String[2];
 		try {
-			fis = new FileInputStream(accessKey);
-			ois = new ObjectInputStream(fis);
-			
+			FileReader accessKey = new FileReader(new File(keyAddress));
+			bufferReader = new BufferedReader(accessKey);
+			String s;
+			while ((s = bufferReader.readLine()) != null) {
+			String[] split = s.split(" ");
+			String accessKeyId = split[0].split("=")[1];
+			String accessKeyPW = split[1].split("=")[1];
+			accessKeyList[0] = accessKeyId;
+			accessKeyList[1] = accessKeyPW;
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-	//Access Key load into Spring for AWS S3
-	public String[] getAccessKey(){
-		String keys[] = null;
-		try {
-			keys = (String[]) ois.readObject();
-			System.out.println(keys.toString());
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return keys;
+		return accessKeyList;
 	}
 	
 }
